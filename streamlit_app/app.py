cc0f3ffb51dff45595a2271067bd3dfb969ad93d
Rohan Jairam <58042335+rohanj12@users.noreply.github.com>
2025-08-19 03:59:24 +0530

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
-from src.llm_engine import generate_code_from_query
-from src.chart_generation import generate_chart_from_query
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
+from src.llm_engine import generate_code_from_query
+from src.chart_generation import generate_chart_from_query
 from PIL import Image
 
 # ----------------- PAGE CONFIG -------------------
